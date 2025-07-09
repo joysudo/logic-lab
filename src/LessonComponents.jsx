e0b16de089154db17cf843937a44abd49edccb77
Joy <joysu2008@gmail.com>
2025-07-08 20:56:23 -0400

    Add multiple choice question component

--- src/LessonComponents.jsx
+++ src/LessonComponents.jsx
@@ -7,13 +7,31 @@ export function BodyText({ text }) {
 }
 
 import { useState } from 'react';
-export function RevealSlide({ before, after }) {
+export function Reveal({ before, after }) {
   const [revealed, setRevealed] = useState(false);
   return (
     <>
       <p>{before}</p>
       {!revealed && <button onClick={() => setRevealed(true)}>Reveal</button>}
       {revealed && <p className="revealed-text">{after}</p>}
+    </>
+  );
+}
+
+export function Question ({type, question, answers, correct}) {
+  const [selectedOption, setSelectedOption] = useState("unselected");
+  return(
+    <>
+      <p>{question}</p>
+      <div className="options">
+        {answers.map(([answer, explanation], index) => (
+          <label>
+            <input type={type} checked={selectedOption === index} onChange={() => setSelectedOption(index)}/>
+            {answer}
+          </label>
+        ))}
+      </div>
+      {typeof selectedOption === "number" && answers[selectedOption] && (selectedOption === correct ? (<p className="correct-answer">{answers[selectedOption][1]}</p>) : (<p className="incorrect-answer">{answers[selectedOption][1]}</p>))}
     </>
   );
 }
--- src/LessonContent.jsx
+++ src/LessonContent.jsx
@@ -1,23 +1,24 @@
-import { Title, BodyText, RevealSlide } from './LessonComponents.jsx';
+import { Title, BodyText, Reveal, Question } from './LessonComponents.jsx';
 
 const lessonContent = [
   {
     title: "Sample Lesson 1",
     slides: [
       [<Title text="Straw Man"/>, <BodyText text="this is some sample lesson 1 body text"/>],
-      [<RevealSlide before="reveal slide text" after="this is after the reveal"/>]
+      [<Reveal before="reveal slide text" after="this is after the reveal"/>],
+      [<Question type="radio" question="this is a sample question" answers={[["this is the first answer option", "explanation"], ["dos", "fsdsd"], ["third", "dsfjkis"], ["this is the fourth", "sdfsdfsd"]]} correct={1}/>]
     ]
   }, {
     title: "Sample Lesson 2",
     slides: [
       [<Title text="Red Herring"/>, <BodyText text="this is some sample lesson 2 body text"/>],
-      [<RevealSlide before="reveal slide text" after="this is after the reveal"/>]
+      [<Reveal before="reveal slide text" after="this is after the reveal"/>]
     ]
   }, {
     title: "Sample Lesson 3",
     slides: [
       [<Title text="False Dichotomy"/>, <BodyText text="this is some sample lesson 3 body text"/>],
-      [<RevealSlide before="reveal slide text" after="this is after the reveal"/>]
+      [<Reveal before="reveal slide text" after="this is after the reveal"/>]
     ]
   },
 ];
