d0959e22445bea94eb5df07bcccef7a30541a12a
Joy <joysu2008@gmail.com>
2025-07-18 15:20:52 -0400

    Implement lesson locking and style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,10 +24,27 @@ export default function Home() {
         <div className="lessons-column"> 
           {/* iterates through lessons, turning to jsx table of contents entries*/}
           {lessonContent.map((lesson, index) => ( 
+            // parseInt(localStorage.getItem("completedLessonIndex") || "0", 10)
             <div key={index} className="lesson-card">
-              <a href={`/lesson/${index + 1}`} className="lesson-card-title">{lesson.title}</a>
-              {!imageErrors[index + 1] ? (<img src={`/images/card${index + 1}.png`} className="lesson-card-image" onError={() => handleImageError(index + 1)}/>)
-              : (<p>joy felt too lazy to put an image here</p>)}
+              {index > parseInt(localStorage.getItem("completedLessonIndex") || "0", 10) && 
+                <div className="lesson-card-locked">
+                  <img src={`/images/sample-card-back.png`}></img>
+                  <h1>{lesson.title}</h1>
+                </div>
+              }
+              {index === parseInt(localStorage.getItem("completedLessonIndex") || "0", 10) && 
+                <div className="lesson-card-play">
+                  <a href={`/lesson/${index + 1}`} className="lesson-card-play-title">{lesson.title}</a>
+                  <a href={`/lesson/${index + 1}`} className="lesson-card-play-button"></a>
+                </div>
+              }
+              {index < parseInt(localStorage.getItem("completedLessonIndex") || "0", 10) && 
+                <div className="lesson-card-unlocked">
+                  <a href={`/lesson/${index + 1}`} className="lesson-card-title">{lesson.title}</a>
+                  {!imageErrors[index + 1] ? (<img src={`/images/card${index + 1}.png`} className="lesson-card-image" onError={() => handleImageError(index + 1)}/>)
+                  : (<p>joy felt too lazy to put an image here</p>)}
+                </div>
+              }
             </div>
           ))}
         </div>
