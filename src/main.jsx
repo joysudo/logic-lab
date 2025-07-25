e6018f24f5ff60fd39a74593450223aad3e1fd3f
Joy <joysu2008@gmail.com>
2025-07-25 15:25:16 -0400

    Reconfigure to use separate matching.css file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './styles.css';
+import './css/styles.css';
+import './css/matching.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
