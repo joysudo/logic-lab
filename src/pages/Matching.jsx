09cd3d02173b68dfde0720f11f416b2f738b1c90
Joy <joysu2008@gmail.com>
2025-07-25 17:32:27 -0400

    Create basic adaptable card layout and style

--- src/pages/Matching.jsx
+++ src/pages/Matching.jsx
@@ -1,6 +1,45 @@
+import { useState, useEffect } from 'react';
+import { lessonContent} from '../LessonContent.jsx';
+
 export default function Matching() {
-    return (
-        <>
-        </>
-    );
-}
+  const fallacies = lessonContent.slice(2).map(obj => ({name: obj.title, definition: obj.definition}));
+  const [cards, setCards] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+  const [matched, setMatched] = useState([])
+
+  // initialize cards
+  useEffect( () => {
+    const cardPairs = [];
+    fallacies.forEach((fallacy, index) => {
+      cardPairs.push({id: 2*index, content: fallacy.name, id: index}) // ids used to identify matched cards later 
+      cardPairs.push({id: 2*index+1, content: fallacy.definition, id: index})
+    })
+    setCards([...cardPairs].sort(() => Math.random() - 0.5));
+  }, []); // only runs when initialized bc dependency array is empty
+
+  const handleCardClick = (id) => {
+    if (flipped.length === 2 || matched.includes(id)) return;
+    // fill with functionality later
+  };
+
+  return (
+    <div className="matching-container">
+      <div className="matching-info">
+        <h2 className="matching-timer">Time: {null}</h2> {/*later issue*/}
+        <h2 className="matching-timer">Best time: {null}</h2> {/*later: {bestTime !== null && ...}*/}
+      </div>
+      <div className="matching-card-grid">
+        {cards.map(card => (
+          <div
+            key={card.id}
+            onClick={() => handleCardClick(card.id)}
+            className={`matching-card ${flipped.includes(card.id) ? "flipped" : ""}`}
+          >
+            <div className="matching-card-front">?</div>
+            <div className="matching-card-back">{card.content}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
