4660c5eff0af9fbded3527e0ed33e6096d39fcde
Joy <joysu2008@gmail.com>
2025-07-26 14:43:22 -0400

    Add basic timer functionality

--- src/pages/Matching.jsx
+++ src/pages/Matching.jsx
@@ -5,7 +5,9 @@ export default function Matching() {
   const fallacies = lessonContent.slice(2).map(obj => ({name: obj.title, definition: obj.definition}));
   const [cards, setCards] = useState([]);
   const [flipped, setFlipped] = useState([]);
-  const [matched, setMatched] = useState([])
+  const [matched, setMatched] = useState([]);
+  const [time, setTime] = useState(0);
+  const [gameStarted, setGameStarted] = useState(false);
 
   // initialize cards
   useEffect( () => {
@@ -17,6 +19,14 @@ export default function Matching() {
     setCards([...cardPairs].sort(() => Math.random() - 0.5));
   }, []); // only runs when initialized bc dependency array is empty
 
+  useEffect (() => {
+    let interval;
+    if (gameStarted && matched.length < fallacies.length * 2) {
+      interval = setInterval(() => setTime(t => t+1), 1000);
+    }
+    return () => clearInterval(interval);
+  }, [flipped, matched]);
+
   const resetGame = () => {
     const cardPairs = [];
     fallacies.forEach((fallacy, index) => {
@@ -26,9 +36,11 @@ export default function Matching() {
     setCards([...cardPairs].sort(() => Math.random() - 0.5));
     setFlipped([]);
     setMatched([]);
+    setTime(0);
   };
 
   const handleCardClick = (id) => {
+    if (!gameStarted) setGameStarted(true);
     if (flipped.length === 2 || matched.includes(id)) return;
     const newFlipped = [...flipped, id]  // need this; variables update instaneously but useState doesn't
     setFlipped(newFlipped);
@@ -48,8 +60,8 @@ export default function Matching() {
       {matched.length !== fallacies.length * 2 &&
         <>
           <div className="matching-info">
-            <h2 className="matching-timer">Time: {null}</h2> {/*later issue*/}
-            <h2 className="matching-timer">Best time: {null}</h2> {/*later: {bestTime !== null && ...}*/}
+            <h2 className="matching-timer">Time: {`${String(Math.floor(time / 60)).padStart('0')}:${String(time % 60).padStart(2, '0')}`}</h2>
+            <h2 className="matching-timer">Best time: {null}</h2> 
           </div>
           <div className="matching-card-grid">
             {cards.map(card => (
@@ -68,6 +80,7 @@ export default function Matching() {
       {matched.length === fallacies.length * 2 &&
         <>
          <h1>You won!</h1>
+         <p>Your time was {`${String(Math.floor(time / 60)).padStart('0')}:${String(time % 60).padStart(2, '0')}`}.</p>
          <button onClick={resetGame} className="matching-button-replay matching-button">Play Again?</button>
          <a href={`/`} className="matching-button">Back to Home</a>
         </>
